00aae5f2079d493baa99031a58491833e34d632b
Lucas Henrique de Oliveira Matos <lucashenrique.mtos@gmail.com>
2023-02-16 20:35:49 -0300

    Corrigindo path

--- src/main/java/br/com/med/voll/api/Controller/MedicoController.java
+++
@@ -1,9 +0,0 @@
-package br.com.med.voll.api.Controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("medicos")
-public class MedicoController {
-}

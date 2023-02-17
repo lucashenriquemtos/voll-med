d26516ed549ab6a7e4dd9a48551f27390ecba84a
Lucas Henrique de Oliveira Matos <lucashenrique.mtos@gmail.com>
2023-02-16 22:32:20 -0300

    Adicionando Validations ao MedicoDTO

--- src/main/java/br/com/med/voll/api/medico/MedicoDTO.java
+++ src/main/java/br/com/med/voll/api/medico/MedicoDTO.java
@@ -1,6 +1,26 @@
 package br.com.med.voll.api.medico;
 
 import br.com.med.voll.api.endereco.EnderecoDTO;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
-public record MedicoDTO(String nome, String email, String crm, Especialidade especialidade, EnderecoDTO enderecoDTO) {
+public record MedicoDTO(
+        @NotBlank
+        String nome,
+        @NotBlank
+        @Email
+        String email,
+        @NotBlank
+        String telefone,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
+        String crm,
+        @NotNull
+        Especialidade especialidade,
+        @NotNull
+        @Valid
+        EnderecoDTO endereco) {
 }

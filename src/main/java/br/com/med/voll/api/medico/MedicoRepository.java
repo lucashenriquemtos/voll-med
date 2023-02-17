0975ddb613218a628288ccab91a82d1e8c1c10bb
Lucas Henrique de Oliveira Matos <lucashenrique.mtos@gmail.com>
2023-02-16 21:20:17 -0300

    Adicionando repository

--- src/main/java/br/com/med/voll/api/medico/MedicoRepository.java
+++ src/main/java/br/com/med/voll/api/medico/MedicoRepository.java
@@ -1,7 +1,9 @@
 package br.com.med.voll.api.medico;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MedicoRepository extends JpaRepository<Medico, Long> {
 
 }

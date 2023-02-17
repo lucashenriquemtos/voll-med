00652ccc293cc341143cfdea05c74c5bbd24412c
Lucas Henrique de Oliveira Matos <lucashenrique.mtos@gmail.com>
2023-02-16 22:27:37 -0300

    Adicionando imports na em Endereco.java

--- src/main/java/br/com/med/voll/api/endereco/Endereco.java
+++ src/main/java/br/com/med/voll/api/endereco/Endereco.java
@@ -1,8 +1,14 @@
 package br.com.med.voll.api.endereco;
 
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Endereco {
 
     private String logradouro;
